fbd8df8e42e6fcf2fdb42584719af755805bdd03
Vishaljha12 <91270374+Vishaljha12@users.noreply.github.com>
2026-02-23 19:14:41 +0530

    THIS IS

--- script.js
+++ script.js
@@ -1,2 +1,81 @@
-let A="Hello World"
-console.log(A)
+// var count
+// document.write("Starting loop")
+// document.write("<br/>")
+// for(count=0;count<10;count++){
+//     document.write(count)
+//     document.write("</br>")
+// }
+// document.write("LOOP STOPED")
+// var b=5
+// console.log(b)
+// console.log(typeof(b))
+// b=false
+// console.log(b)
+// console.log(typeof(b))
+// b="HELLO"
+// console.log(b)
+// console.log(typeof(b))
+// b=null
+// console.log(b)
+// console.log(typeof(b))
+// var k
+// console.log(k)
+// console.log(typeof(k))
+
+// //Function
+// var sayHello= function(){
+//     alert('Hello world')
+// }
+
+// //array
+// var number=[1,2,3,4]
+// var animals=newArray("cat","dog","rat")
+
+// //object
+// var person={
+
+// }
+
+// const symb1=Symbol(4)
+// console.log(symb1)
+// const symb2=Symbol(4)
+// console.log(symb2)
+// if(symb1==symb2){
+//     console.log("YES")
+// }
+// else{
+//     console.log("NO")
+// }
+var a=300,b=10,c=90,linebreak="<br>"
+// document.write("a+b+c=")
+// var result=a+b+c;
+// document.write(result)
+// document.write(linebreak)
+
+// document.write("a+b=")
+//     result=a+b
+// document.write(result)
+// document.write(linebreak)
+
+// var a=8 , b='8';
+// var result=a==b
+// document.write(linebreakt)
+// document.write(result)
+// var result=a===b
+// document.write(linebreak)
+// document.write(result)
+// var result=a>=b
+// document.write(linebreak)
+// document.write(result)
+// var result=a<=b
+// document.write(linebreak)
+// document.write(result)
+
+const person={
+    name:"AJAY",
+    lname:"KK",
+    age:15
+}
+for(let x in person){
+    console.log("PERSON DETAIL:" + x + " "+ person[x])
+}
