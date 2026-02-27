894ba8e15078181092a6d1f92eb3dd8919367a71
Vishaljha12 <91270374+Vishaljha12@users.noreply.github.com>
2026-02-27 10:01:45 +0530

    CLASS WORK

---
+++ script.js
@@ -0,0 +1,141 @@
+// var count
+// document.write("Starting loop")
+// document.write("<br/>")
+// for(count=0;count<10;count++){
+//     document.write(count)
+//     document.write("</br>")
+// }
+// document.write("LOOP STOPED")
+// var b=5
+// console.log(b)
+// console.log(typeof(b))
+// b=false
+// console.log(b)
+// console.log(typeof(b))
+// b="HELLO"
+// console.log(b)
+// console.log(typeof(b))
+// b=null
+// console.log(b)
+// console.log(typeof(b))
+// var k
+// console.log(k)
+// console.log(typeof(k))
+
+// //Function
+// var sayHello= function(){
+//     alert('Hello world')
+// }
+
+// //array
+// var number=[1,2,3,4]
+// var animals=newArray("cat","dog","rat")
+
+// //object
+// var person={
+
+// }
+
+// const symb1=Symbol(4)
+// console.log(symb1)
+// const symb2=Symbol(4)
+// console.log(symb2)
+// if(symb1==symb2){
+//     console.log("YES")
+// }
+// else{
+//     console.log("NO")
+// }
+var a=300,b=10,c=90,linebreak="<br>"
+// document.write("a+b+c=")
+// var result=a+b+c;
+// document.write(result)
+// document.write(linebreak)
+
+// document.write("a+b=")
+//     result=a+b
+// document.write(result)
+// document.write(linebreak)
+
+// var a=8 , b='8';
+// var result=a==b
+// document.write(linebreakt)
+// document.write(result)
+// var result=a===b
+// document.write(linebreak)
+// document.write(result)
+// var result=a>=b
+// document.write(linebreak)
+// document.write(result)
+// var result=a<=b
+// document.write(linebreak)
+// document.write(result)
+
+// const person={
+//     name:"AJAY",
+//     lname:"KK",
+//     age:15
+// }
+// for(let x in person){
+//     console.log("PERSON DETAIL:" + x + " "+ person[x])
+
+// }
+// for(let j in person){
+//         console.log("PERSON DETAIL:" + j + " "+ person[j])
+
+// }
+
+// var count=0;
+// document.write("STARTING LOOP")
+
+// while(count<10){
+//     document.write("Current count:"+count+"</br>")
+//     count++;
+// }
+// document.write("LOOP STOPED")
+
+// var age=17;
+// if(age>18){
+//     document.write("You are major")
+// }
+// else{
+//     document.write("YOU ARE MINOR")
+// }
+
+// let grade="A"
+
+// switch(grade){
+//     case "A":document.write("GOOD JOB </br>")
+//     break;
+
+//     case "B":document.write("Pretty good </br>")
+//     break;
+
+//     case "C":document.write("Passed</br>")
+//     break;
+
+//     case "D":document.write("Not so good</br>")
+//     break;
+
+//     case "F":document.write("Failed</br>")
+//     break;
+
+//     default:document.write("Unknown grade</br>")
+// }
+// document.write("EXITTING SWITCH CASE");
+
+// function sayHello(name,age){
+//     document.write(name+" is "+age+" year old")
+// }
+
+// var res=mul(12,30)
+
+// function mul(x,y){
+//     return x*y;
+// }
+// document.write("RESULT OF mul IS:"+res)
+var add2=(a,b) =>{
+    console.log(a+b);
+}
+
+add2(102,20)
