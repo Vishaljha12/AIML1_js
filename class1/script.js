c52f068f8ad652c17d077f0766baf582dc261a60
VISHAL KUMAR JHA <kjvishal336@gmail.com>
2026-03-06 02:50:33 +0530

    all files

---
+++ class1/script.js
@@ -0,0 +1,64 @@
+// var b=5
+// console.log(b)
+// console.log(typeof(b))
+// b=false
+// console.log(b)
+// console.log(typeof(b))
+// b="HELLO"
+// console.log(b)
+// console.log(typeof(b))
+// b=null
+// console.log(b)
+// console.log(typeof(b))
+// var k
+// console.log(k)
+// console.log(typeof(k))
+
+// //Function
+// var sayHello= function(){
+//     alert('Hello world')
+// }
+
+// //array
+// var number=[1,2,3,4]
+// var animals=newArray("cat","dog","rat")
+
+// //object
+// var person={
+
+// }
+
+// const symb1=Symbol(4)
+// console.log(symb1)
+// const symb2=Symbol(4)
+// console.log(symb2)
+// if(symb1==symb2){
+//     console.log("YES")
+// }
+// else{
+//     console.log("NO")
+// }
+var a=300,b=10,c=90,linebreak="<br>"
+// document.write("a+b+c=")
+// var result=a+b+c;
+// document.write(result)
+// document.write(linebreak)
+
+// document.write("a+b=")
+//     result=a+b
+// document.write(result)
+// document.write(linebreak)
+
+// var a=8 , b='8';
+// var result=a==b
+// document.write(linebreakt)
+// document.write(result)
+// var result=a===b
+// document.write(linebreak)
+// document.write(result)
+// var result=a>=b
+// document.write(linebreak)
+// document.write(result)
+// var result=a<=b
+// document.write(linebreak)
+// document.write(result)
