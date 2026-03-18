85702462c5e27ade14df0ea6ef26ba2637a6b556
VISHAL KUMAR JHA <kjvishal336@gmail.com>
2026-03-18 10:54:29 +0530

    this is class work

--- class2/script.js
+++ class2/script.js
@@ -1,166 +1,42 @@
-// var count
-// document.write("Starting loop")
-// document.write("<br/>")
-// for(count=0;count<10;count++){
-//     document.write(count)
-//     document.write("</br>")
+// let para= document.getElementById("mypara");
+// let text= para.getAttribute("class");
+// document.getElementById("head").innerHTML=text;
+
+
+// function change(){
+//     document.getElementById("head").setAttribute("class","hi");
+//     document.getElementById("head").innerHTML="Class is changed";
 // }
-// document.write("LOOP STOPED")
-// var b=5
-// console.log(b)
-// console.log(typeof(b))
-// b=false
-// console.log(b)
-// console.log(typeof(b))
-// b="HELLO"
-// console.log(b)
-// console.log(typeof(b))
-// b=null
-// console.log(b)
-// console.log(typeof(b))
-// var k
-// console.log(k)
-// console.log(typeof(k))
+// document.getElementById("head").style.color="red";
+// const heading=document.createElement("h2");
+// const bodytag=document.querySelector("body");
+// bodytag.prepend(heading);
+// bodytag.remove(heading);
+// heading.innerHTML="This is heading 2";/
+function change(){
+document.open();
+document.write("<h1>Document is changed</h1>");
+document.close();
+}
+function color()
+{
+    document.getElementById("head").style.color="blue";
+}
+function backcolor()
+{
+    document.getElementById("head").style.color="red";
+}
+function cha(){
+    document.getElementById("head").innerHTML="Double click is done";
+}
 
-// //Function
-// var sayHello= function(){
-//     alert('Hello world')
-// }
+let x=document.getElementById("oo");
+x.addEventListener("click",chan)
+function chan(){
+    document.getElementById("head").innerHTML="Event is added";
+}
 
-// //array
-// var number=[1,2,3,4]
-// var animals=newArray("cat","dog","rat")
-
-// //object
-// var person={
-
-// }
-
-// const symb1=Symbol(4)
-// console.log(symb1)
-// const symb2=Symbol(4)
-// console.log(symb2)
-// if(symb1==symb2){
-//     console.log("YES")
-// }
-// else{
-//     console.log("NO")
-// }
-var a=300,b=10,c=90,linebreak="<br>"
-// document.write("a+b+c=")
-// var result=a+b+c;
-// document.write(result)
-// document.write(linebreak)
-
-// document.write("a+b=")
-//     result=a+b
-// document.write(result)
-// document.write(linebreak)
-
-// var a=8 , b='8';
-// var result=a==b
-// document.write(linebreakt)
-// document.write(result)
-// var result=a===b
-// document.write(linebreak)
-// document.write(result)
-// var result=a>=b
-// document.write(linebreak)
-// document.write(result)
-// var result=a<=b
-// document.write(linebreak)
-// document.write(result)
-
-// const person={
-//     name:"AJAY",
-//     lname:"KK",
-//     age:15
-// }
-// for(let x in person){
-//     console.log("PERSON DETAIL:" + x + " "+ person[x])
-
-// }
-// for(let j in person){
-//         console.log("PERSON DETAIL:" + j + " "+ person[j])
-
-// }
-
-// var count=0;
-// document.write("STARTING LOOP")
-
-// while(count<10){
-//     document.write("Current count:"+count+"</br>")
-//     count++;
-// }
-// document.write("LOOP STOPED")
-
-// var age=17;
-// if(age>18){
-//     document.write("You are major")
-// }
-// else{
-//     document.write("YOU ARE MINOR")
-// }
-
-// let grade="A"
-
-// switch(grade){
-//     case "A":document.write("GOOD JOB </br>")
-//     break;
-
-//     case "B":document.write("Pretty good </br>")
-//     break;
-
-//     case "C":document.write("Passed</br>")
-//     break;
-
-//     case "D":document.write("Not so good</br>")
-//     break;
-
-//     case "F":document.write("Failed</br>")
-//     break;
-
-//     default:document.write("Unknown grade</br>")
-// }
-// document.write("EXITTING SWITCH CASE");
-
-// function sayHello(name,age){
-//     document.write(name+" is "+age+" year old")
-// }
-
-// var res=mul(12,30)
-
-// function mul(x,y){
-//     return x*y;
-// }
-// document.write("RESULT OF mul IS:"+res)
-// var add2=(a,b) =>{
-//     console.log(a+b);
-// }
-
-// add2(102,20)
-
-// var person={
-//     name: "vishal",
-//     age:30,
-//     isMarried:true,
-//     address:{
-//         street:'vijay nagar',
-//         flatNo:201
-//     }
-// }
-// console.log(person.address)
-
-// for(let x in person){
-//     console.log(x + " is " + person[x] )
-// }
-var person2=new Object()
-
-person2.name="JIJJI"
-person2.age=55
-person2.isMarried=true
-person2.address={}
-person2.address.flatNO=441
-person2.address.street="VIJAY"
-
-console.log(person2.name,person2.age,person2.isMarried,person2.address.street,person2.address.flatNO)
+function bg(){
+    alert("Welcome to my website");
+    x.style.backgroundColor="green";
+}
---
+++ extras/tictactoe.js
@@ -0,0 +1,44 @@
+      const game = document.getElementById('game');
+let currentPlayer = 'X';
+let board = Array(9).fill(null);
+
+function createBoard() {
+  for (let i = 0; i < 3; i++) {
+    const row = game.insertRow();
+    for (let j = 0; j < 3; j++) {
+      const cell = row.insertCell();
+      cell.addEventListener('click', () => makeMove(i * 3 + j));
+    }
+  }
+}
+
+function makeMove(index) {
+  if (board[index] || checkWin()) return;
+  board[index] = currentPlayer;
+  game.rows[Math.floor(index / 3)].cells[index % 3].textContent = currentPlayer;
+  if (checkWin()) {
+    document.getElementById('result').textContent = `${currentPlayer} wins!`;
+    return;
+  }
+  if (board.every(cell => cell)) {
+    document.getElementById('result').textContent = 'Draw!';
+    return;
+  }
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
+
+function checkWin() {
+  const wins = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+  ];
+  for (let win of wins) {
+    if (board[win[0]] && board[win[0]] === board[win[1]] && board[win[0]] === board[win[2]]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+createBoard();
